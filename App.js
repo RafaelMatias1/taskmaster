889ee5b19dfa8e21b0b6900b10eb9effaeb33501
Rafael Matias <111144446+RafaelMatias1@users.noreply.github.com>
2025-05-22 20:13:55 -0300

    22/05

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthProvider, useAuth } from './src/contexts/AuthContext';
-import { TaskProvider } from './src/contexts/TaskContext';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import TaskFormScreen from './src/screens/TaskFormScreen';
+import { TaskProvider } from './src/contexts/TaskContext';
 import ProfileScreen from './src/screens/ProfileScreen';
 
 // Cria o stack de navegação
@@ -45,8 +45,8 @@ function Routes() {
       {user ? (
         // Rotas privadas (usuário autenticado)
         <>
+          <Stack.Screen name="Home" component={HomeScreen} />        
           <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen 
             name="AddTask" 
             component={TaskFormScreen} 
